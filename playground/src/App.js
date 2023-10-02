0ef008b81a787cbf3629c6217e31af082d625e23
Jeremy2710 <Jeremy2710>
2023-10-01 22:47:00 -0300

    edicion del texto de bienvenida

--- playground/src/App.js
+++ playground/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pendiente de edicion
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Jeremy2710/Proyecto-React/tree/main"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Repositorio de Git
         </a>
       </header>
     </div>
