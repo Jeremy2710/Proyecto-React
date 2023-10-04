a08826407ee5613b08eb29203104ddaefe3a3182
Jeremy2710 <Jeremy2710>
2023-10-04 00:43:52 -0300

    Agregado inicial del carrousel e iconos a public

---
+++ planetaninez/src/app/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <img src='./favicon.ico' width={35} height={35} ></img>
+    </div>
+  )
+}
+
+export default Navbar
