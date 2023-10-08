bb61f81123d860417e2e6f7404cbdfa18aae272a
Nahuel-Zabala <nahuelzabala898@gmail.com>
2023-10-08 12:03:17 -0300

    Implementacion de container, row y col a page js

--- planetaninez/src/app/components/Servicios.jsx
+++ planetaninez/src/app/components/Servicios.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
-function Servicios(){
-    return(
-        <div>Servicios</div>
+function Servicios() {
+
+
+
+
+    return (
+        <div>
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, quis asperiores esse itaque atque, repudiandae saepe quos vel ipsam quo ipsum ratione odit. Asperiores excepturi illum distinctio enim sed iusto.</p>
+
+
+        </div>
     )
 }
 
