90f0749be567fa13977b15916e6e01030a3798f2
Nahuel-Zabala <nahuelzabala898@gmail.com>
2023-10-08 12:31:49 -0300

    Creacion de la base de componentes servicios

--- planetaninez/src/app/components/Servicios.jsx
+++ planetaninez/src/app/components/Servicios.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 
 function Servicios() {
 
@@ -15,7 +17,33 @@ function Servicios() {
                 <h2>Servicios</h2>
                 <Row>
 
-                    <Col>
+                    <Col xs={3}>
+                        <div>
+                            <img src="" alt="" />
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et repudiandae aliquam quibusdam, totam excepturi qui voluptas deleniti odio deserunt vel?</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Container>
+                <Row>
+                    <Col xs={12} sm={4}>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Pase de ...</Card.Title>
+                                <Card.Text>
+                                    <ul>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                        <li></li>
+                                    </ul>
+                                </Card.Text>
+                                <Button>Comprar</Button>
+                            </Card.Body>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
