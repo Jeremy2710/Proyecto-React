8c7b6ed80b2c9a3c52edade780f822df13bff6e4
Jeremy2710 <Jeremy2710>
2023-10-09 21:15:05 -0300

    modificando page

--- planetaninez/src/app/page.js
+++ planetaninez/src/app/page.js
@@ -1,5 +1,3 @@
-// import Image from 'next/image'
-// import styles from './page.module.css'
 import UncontrolledCarrousel from './components/Carrousel/UncontrolledCarrousel';
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
