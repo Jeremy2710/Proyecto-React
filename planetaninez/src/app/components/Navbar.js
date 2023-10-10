3eb011677f3e0165a10c6f78dc49d7d6ac16949c
Jeremy2710 <Jeremy2710>
2023-10-09 21:40:49 -0300

    modificando la ruta de la img de la nav

--- planetaninez/src/app/components/Navbar.js
+++ planetaninez/src/app/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className='Navbar'>
-        <img src='./planetaninez.ico' width={70} height={70} ></img>
+        <img src= '../../../public/planetaninez.ico' width={70} height={70} ></img>
     </div>
   )
 }
