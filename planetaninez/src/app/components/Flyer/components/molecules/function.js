cce9f83bbc5480b609353ae4bca54f62eb20745e
Jeremy2710 <francosilvazegarra@gmail.com>
2023-11-13 23:26:32 -0300

    ARMANDO la beta-main

--- planetaninez/src/app/components/Flyer/components/molecules/function.js
+++
@@ -1,7 +0,0 @@
-
-function mostrarTarjeta(numero) {
-    document.querySelectorAll('.tarjeta').forEach(function (tarjeta) {
-        tarjeta.style.display = 'none';
-    });
-    document.getElementById('tarjeta' + numero).style.display = 'block';
-}
